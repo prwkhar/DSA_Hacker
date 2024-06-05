08c6ece5eebca21f1dff4b3b3e1b073bb4ff2389
prwkhar <prakharagarwal1434@gmail.com>
2024-06-05 12:00:18 +0530

    commit

---
+++ DataStructures/LinkedList/MergePoint/readme.cpp
@@ -0,0 +1,5 @@
+for avoinding the nested loop we are using two pointers and because of that we 
+are traversing at the same time and due to which when we encounter the same node
+we understand teh position where it has been duplicated and then reuturn the data of 
+that node its a good question according to me 
+because earlier i was thinking something else
