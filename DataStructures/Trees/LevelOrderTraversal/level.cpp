49031d313cccaaac2b6d6782f7e71e5aad8f8495
prwkhar <prakharagarwal1434@gmail.com>
2024-06-11 09:33:41 +0530

    commit

---
+++ DataStructures/Trees/LevelOrderTraversal/level.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        if(root==nullptr)
+        return;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            Node* node=q.front();
+            cout<<node->data<<" ";
+            q.pop();
+            if(node->left!=nullptr)
+            q.push(node->left);
+            if(node->right!=nullptr)
+            q.push(node->right);
+        }
+
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.levelOrder(root);
+    return 0;
+}
